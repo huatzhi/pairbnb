e706b8612222b89855ca2166baeee3d346a489b9
huatzhi <huatzhi@gmail.com>
2016-11-23 18:32:50 +0800

    backup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,10 @@ class UsersController < Clearance::UsersController
 
     if @user.save
       #read the params here, and choose to change it to host or stay tenant
-
+      if params['role'].to_i == 1
+        @user.host!
+        @user.save!
+      end
       sign_in @user
       redirect_back_or url_after_create
     else

e36871239d612601eca966efbb5d33fa7424097d
huatzhi <huatzhi@gmail.com>
2016-11-25 10:04:52 +0800

    add avatar column into users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161125003909) do
+ActiveRecord::Schema.define(version: 20161125020319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20161125003909) do
     t.string   "remember_token",     limit: 128,             null: false
     t.string   "first_name"
     t.integer  "role",                           default: 0
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree

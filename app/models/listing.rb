f192f48273ea838e213dec029359887aac44071c
huatzhi <huatzhi@gmail.com>
2016-11-29 16:57:03 +0800

    done

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,10 +5,10 @@ class Listing < ActiveRecord::Base
   include PgSearch
   pg_search_scope :search, against: [:country, :city, :title, :description, :rules, :note],
     using: {tsearch: {dictionary: "english"}}
-  scope :price_min, -> (price_min) {where ("price_per_night >= ?", price_min)}
-  scope :price_max, -> (price_max) {where ("price_per_night <= ?", price_max)}
-  scope :limit_city -> (city) {where ('city like ?', "%#{city}%")}
-  scope :limit_country -> (country) {where ('country like ?', "%#{country}%")}
+  scope :price_min, -> (price_min) {where("price_per_night >= ?", price_min)}
+  scope :price_max, -> (price_max) {where("price_per_night <= ?", price_max)}
+  scope :limit_city, -> (city) {where("city like ?", "%#{city}%")}
+  scope :limit_country, -> (country) {where("country like ?", "%#{country}%")}
 
   def self.text_search(query)
     if query.present?
